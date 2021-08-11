cb6cd14fc16cf69a1f9c044b791c6d3410213dd4
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2021-08-11 23:55:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -190,8 +190,8 @@ async def delete(bot, message):
 async def bot_info(bot, message):
     buttons = [
         [
-            InlineKeyboardButton('Update Channel', url='https://t.me/Mo_Tech_YT'),
-            InlineKeyboardButton('Video', url=f'{TUTORIAL}')
+            InlineKeyboardButton('🧑‍💻 Master', url='https://t.me/Hacker_Jr'),
+            InlineKeyboardButton('Master Info ⚜️', url='https://t.me/Hacker_Jr_Info')
         ]
         ]
     await message.reply(text=f"<b>Developer : <a href='https://t.me/subinps_bot'>SUBIN</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='{TUTORIAL}'>Click here</a>\nUpdate Channel : <a href='https://t.me/Mo_Tech_YT'>Mo Tech YT</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
