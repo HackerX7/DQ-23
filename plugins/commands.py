a3f6aa4557d77334a04b22bc52722cae39b97c73
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2021-08-12 00:00:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -194,5 +194,5 @@ async def bot_info(bot, message):
             InlineKeyboardButton('Master Info ⚜️', url='https://t.me/Hacker_Jr_Info')
         ]
         ]
-    await message.reply(text=f"<b>Developer : <a href='https://t.me/subinps_bot'>SUBIN</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='{TUTORIAL}'>Click here</a>\nUpdate Channel : <a href='https://t.me/Mo_Tech_YT'>Mo Tech YT</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+    await message.reply(text=f"<b>no one gonna help you 🤣🤣\nContact ▷ @Hacker_Jr\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='{TUTORIAL}'>Click here</a>\nUpdate Channel : <a href='https://t.me/Mo_Tech_YT'>Mo Tech YT</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
