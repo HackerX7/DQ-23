023861feba67295148fd6e98fc4967a9133c3c6d
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2021-08-12 18:08:46 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -173,7 +173,7 @@ def get_size(size):
     """Get size in readable format"""
 
     units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
-    size = 📂float(size)
+    size = float(size)
     i = 0
     while size >= 1024.0 and i < len(units):
         i += 1
